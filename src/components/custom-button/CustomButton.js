74b41339ffd3816451ce371611417bf1abce5b9e
shayan <shayan@firstsource.tech>
2020-07-26 16:07:45 +0430

    add custom button

---
+++ src/components/custom-button/CustomButton.js
@@ -0,0 +1,11 @@
+
+import { observable, when, action } from 'mobx';
+
+export default class ChartSizeStore {
+
+    constructor(mainStore) {
+        this.mainStore = mainStore;
+    }
+
+    fancyMethod = () => {  };
+}
